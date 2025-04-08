5c8732466bdbe151ddc578e6a273fc81d873ab62
Umair <umairzahiraj16@gmail.com>
2025-04-08 19:33:49 +0530

    Read image and read videos

--- read.py
+++ read.py
@@ -1,5 +1,18 @@
 import cv2 as cv
 
-img = cv.imread("Photos/cat.jpg")
-cv.imshow("Blue",img)
-cv.waitKey(0)
+# img = cv.imread("Photos/cat.jpg")
+# cv.imshow("Cat",img)
+# cv.waitKey(0)
+
+capture = cv.VideoCapture("videos/dog.mp4");     # 0 - webcam  , int : camara number
+
+while True:
+    isTrue,frame = capture.read()
+    cv.imshow("Video",frame)
+    if cv.waitKey(20) and 0xFF == ord('d'):  # if 'd' pressed stop displaying
+        break
+
+capture.release()
+cv.destroyWindow()
+
+# after last frame it gives error because there is no more frames to display
