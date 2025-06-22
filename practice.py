a62f7f413625b4938f5b8f2328174196e186a033
Umair <umairzahiraj16@gmail.com>
2025-06-23 00:38:37 +0530

    fornier transformation codes added updated in practice.py

--- practice.py
+++ practice.py
@@ -1,33 +1,43 @@
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
-# Read the image in color
-img = cv2.imread("image/plus.jpg", 1);
-img1 = img.copy()
-img2 = img.copy()
+xray = cv2.imread("image/noisy_xray_image.jpg",0)
 
-img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-rs,thresh = cv2.threshold(img_gray,50,255,cv2.THRESH_BINARY)
-contours ,hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+f = np.fft.fft2(xray)
+fshift = np.fft.fftshift(f)
+magnitude_spectrum = 20 * np.log(np.abs(fshift) + 1)
+rows, cols = xray.shape
+mask = np.ones((rows, cols), dtype=np.uint8)
+r = 6
+center_1 = [338, 240]
+center_2 = [339, 270]
+center_3 = [336, 332]
+center_4 = [338, 361]
 
-cv2.drawContours(img1,contours,-1,(0,255,0),1)
-cv2.drawContours(img2,[contours[2],contours[3]],-1,(255,0,0),2)
-plt.figure(figsize=(10, 6))
+x, y = np.ogrid[:rows, :cols]
+for cx, cy in [center_1, center_2,center_3,center_4]:
+    mask_area = (x - cx)**2 + (y - cy)**2 <= r**2
+    mask[mask_area] = 0
+fshift_filtered = fshift * mask
+magnitude_filtered = 20 * np.log(np.abs(fshift_filtered)
++ 1)
+f_ishift = np.fft.ifftshift(fshift_filtered)
+img_back = np.abs(np.fft.ifft2(f_ishift))
+img_back_normalized = cv2.normalize(img_back, None, 0,
+255, cv2.NORM_MINMAX)
+img_back_uint8 = np.uint8(img_back_normalized)
 
-img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-img1 = cv2.cvtColor(img1,cv2.COLOR_BGR2RGB)
-img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2RGB)
-
-print(len(contours))
-
-plt.subplot(131)
-plt.imshow(img)
-
-plt.subplot(132)
-plt.imshow(img1)
-
-plt.subplot(133)
-plt.imshow(img2)
+plt.subplot(2, 3, 1), plt.imshow(xray, cmap='gray'),
+plt.title("Original")
+plt.subplot(2, 3, 2), plt.imshow(magnitude_spectrum,
+cmap='gray'), plt.title("FFT Magnitude (Before)")
+plt.subplot(2, 3, 3), plt.imshow(mask * 255,
+cmap='gray'), plt.title("Band Reject Mask")
+plt.subplot(2, 3, 4), plt.imshow(magnitude_filtered,
+cmap='gray'), plt.title("FFT Magnitude (After)")
+plt.subplot(2, 3, 5), plt.imshow(img_back_uint8,
+cmap='gray'), plt.title("Denoised Image")
+plt.tight_layout()
 
 plt.show()
-
